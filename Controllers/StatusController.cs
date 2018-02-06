d9150228862b011d5755b1065d46a07f1ac0de65
Tobias Eichner <tobias.eichner@partner.bmw.de>
2018-02-06 09:23:25 +0100

    add controller and endpoints for liveness and readiness probes

---
+++ Controllers/StatusController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GeopositionService.Models;
+using GeopositionService.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeopositionService.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class StatusController : Controller
+    {
+        [HttpGet]
+        [Route("live")]
+        public IActionResult GetLive()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("readiness")]
+        public IActionResult GetReadiness()
+        {
+            return Ok();
+        }
+    }
+}
