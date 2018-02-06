0690c2b877a1269f576ba3990350d4abb6ab9c10
Tobias Eichner <tobias.eichner@partner.bmw.de>
2018-02-06 16:52:47 +0100

    use container port 80 again

--- Program.cs
+++ Program.cs
@@ -19,7 +19,6 @@ namespace GeopositionService
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://*:8080")
                 .UseStartup<Startup>()
                 .Build();
     }
