8720438804408edb007c26b46c093126ad43f210
Tobias Eichner <tobias.eichner@partner.bmw.de>
2018-02-07 14:16:37 +0100

    modify bash script

--- deploy/k8s/nr-deploy-marker.sh
+++ deploy/k8s/nr-deploy-marker.sh
@@ -6,6 +6,12 @@
 #  $3: Build Number
 #  $4: User name (build queued by)
 
+echo "NR APP ID   : $0 "
+echo "NR API key  : $1 "
+echo "SCM revision: $2 "
+echo "Build Number: $3 "
+echo "User name   : $4 "
+
 curl -X POST 'https://api.newrelic.com/v2/applications/$0/deployments.json' \
      -H 'X-Api-Key:$1' -i \
      -H 'Content-Type: application/json' \
@@ -13,7 +19,6 @@ curl -X POST 'https://api.newrelic.com/v2/applications/$0/deployments.json' \
 '{
   "deployment": {
     "revision": "$2",
-    "changelog": "",
     "description": "$3",
     "user": "$4"
   }
