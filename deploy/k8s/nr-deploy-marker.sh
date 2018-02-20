4547c06e24d202ed98babb738cc422a213072e10
Tobias Eichner <tobias.eichner@partner.bmw.de>
2018-02-20 13:40:56 +0100

    update shell script for NR deployment marker

--- deploy/k8s/nr-deploy-marker.sh
+++ deploy/k8s/nr-deploy-marker.sh
@@ -2,15 +2,17 @@
 # Arguments:
 #  $1: NR APP ID
 #  $2: NR API key
-#  $3: SCM revision
-#  $4: Build Number
-#  $5: User name (build queued by)
+#  $3: SCM URI
+#  $4: SCM revision
+#  $5: Build Number
+#  $6: User name (build queued by)
 
 echo "NR APP ID   : $1"
 echo "NR API key  : $2"
-echo "SCM revision: $3"
-echo "Build Number: $4"
-echo "User name   : $5"
+echo "SCM URI     : $3"
+echo "SCM revision: $4"
+echo "Build Number: $5"
+echo "User name   : $6"
 
 curl -X POST 'https://api.newrelic.com/v2/applications/'$1'/deployments.json' \
      -H 'X-Api-Key:'$2 -i \
@@ -18,8 +20,8 @@ curl -X POST 'https://api.newrelic.com/v2/applications/'$1'/deployments.json' \
      -d \
 '{
   "deployment": {
-    "revision": "'$3'",
-    "description": "'$4'",
-    "user": "'$5'"
+    "revision": "'$4'",
+    "description": "Revision in repository: '$3'/commit/'$4', Build Number: '$5'",
+    "user": "'$6'"
   }
 }' 
